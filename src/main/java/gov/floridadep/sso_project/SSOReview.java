4be7ea4669a541ad37372a44e51f2f567def4bba
Cody Keen <codykeen907@gmail.com>
2022-12-18 23:00:29 -0500

    started switch to check for case manager

--- src/main/java/gov/floridadep/sso_project/SSOReview.java
+++ src/main/java/gov/floridadep/sso_project/SSOReview.java
@@ -8,16 +8,13 @@ import java.util.Scanner;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /*
- * Program reads an SSO check sheet and generates an email update for case managers.
- * Only works for .xlsx files with a specific database schema.
- * Code will need to be adapted to any database schema changes.
- * Make sure SSO data is consistent, otherwise code will break!
- * 
+ * Program reads an SSO check sheet and generates an email update for case managers. 
  */
 
 public class SSOReview {
@@ -36,39 +33,8 @@ public class SSOReview {
 		// Get the Sheet object at the given index
 		Sheet firstSheet = workbook.getSheetAt(0);
 		// Returns an iterator of the sheets in the workbook in sheet order
-		Iterator<Row> iterator = firstSheet.iterator();
-				
-/*		//Iterate through each row one by one
-		while (iterator.hasNext()) {
-		Row nextRow = iterator.next();
-		//For each row, iterate through all the columns
-		Iterator<Cell> cellIterator = nextRow.cellIterator();
+		Iterator<Row> rowIterator = firstSheet.iterator();
 					
-			while (cellIterator.hasNext()) {
-				Cell cell = cellIterator.next();
-				//Check the cell type and format accordingly		
-				switch (cell.getCellType()) {
-					case STRING:
-						System.out.print(cell.getStringCellValue());
-						break;
-					case BOOLEAN:
-						System.out.print(cell.getBooleanCellValue());
-						break;
-					case NUMERIC:
-						System.out.print(cell.getNumericCellValue());
-						break;
-					case BLANK:
-						System.out.print("NULL");
-				default:
-						System.out.print("_");
-					break;
-				}
-				System.out.print(" ");
-			}
-			System.out.println();
-		}
-		
-*/		
 		// Ask user to input the row number that the program will start at
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter starting row number");
@@ -77,15 +43,40 @@ public class SSOReview {
 		// For each row (starting at the row specified by the user), do this:
 		for (int rowIndex = scanner.nextInt() - 1; rowIndex <= firstSheet.getLastRowNum(); rowIndex++) {
 			// Return the next row in the iteration
-			Row nextRow = iterator.next();
+			Row nextRow = rowIterator.next();
 			// Return a cell iterator for the row
 			Iterator<Cell> cellIterator = nextRow.cellIterator();
 			// Get the row object at the given index
 			Row row = firstSheet.getRow(rowIndex);
-			// Get cell object at the given index
-			Cell cell = row.getCell(33);
+			// Get cells for the row
+			Cell caseManagerCell = row.getCell(33);
+			Cell dateCell = row.getCell(5);
+			Cell letterCell = row.getCell(28);
+			Cell spillTypeCell = row.getCell(15);
+			Cell spillLocationCell = row.getCell(9);
+			Cell facilityNameCell = row.getCell(3);
+			Cell statusCell = row.getCell(35);
+			// Fetch the string value of the case manager cell
+			DataFormatter formatter = new DataFormatter();
+			String caseManager = formatter.formatCellValue(caseManagerCell);
+			// Check for who the case manager is for the row and act accordingly:
+			switch (caseManager) {
+				case "Carolyn":
+					System.out.println(caseManagerCell);
+					System.out.print(dateCell + " - ");
+					System.out.print(letterCell + " - ");
+					System.out.print(spillTypeCell + " - ");
+					System.out.print(spillLocationCell + " - ");
+					System.out.print(facilityNameCell + " - ");
+					System.out.print(statusCell);
+					System.out.println();
+					break;
+				
+					
+			}
 			
-			System.out.println(cell);
+		//	carolyn, manny, cory, hannah, trey, gina, amanda, shaun, jenny
+			
 		}
 		
 		scanner.close();
